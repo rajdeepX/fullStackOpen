616f318b577605c1e519e96f3d1195c37c88d185
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-07-08 22:37:33 +0530

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: "4545454545"
+     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
   const handleAddName = (e) => {
     setNewName(e.target.value)
@@ -21,11 +24,17 @@ const App = () => {
     }
 
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNum
     }
 
     setPersons([...persons, newPerson ])
     setNewName("")
+    setNewNum("")
+  }
+
+  const handleAddNum = (e) => {
+    setNewNum(e.target.value)
   }
 
   return (
@@ -36,11 +45,14 @@ const App = () => {
           name: <input onChange={handleAddName} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleAddNum} value={newNum} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map( person => <p key={person.name}>{person.name}</p>)}
+      {persons.map( person => <p key={person.name}>{person.name} : {person.number}</p>)}
     </div>
   )
 }
