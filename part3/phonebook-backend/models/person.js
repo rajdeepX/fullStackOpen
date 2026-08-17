04dfaba7826187687224c03a99b711652f690404
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-08-17 16:10:47 +0530

    Revert "update"
    This reverts commit d50338060ce93c64d227dbc5181a77571cf9384d.

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 
-const url = process.env.MONGODB_URI
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+// const name = process.argv[3]
+// const number = process.argv[4]
+
+const url = `mongodb://fullstackopen:${password}@ac-z8l1ddy-shard-00-00.hxndo4v.mongodb.net:27017,ac-z8l1ddy-shard-00-01.hxndo4v.mongodb.net:27017,ac-z8l1ddy-shard-00-02.hxndo4v.mongodb.net:27017/phoneBookApp?ssl=true&replicaSet=atlas-qqtb2i-shard-0&authSource=admin&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
