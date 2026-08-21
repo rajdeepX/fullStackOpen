a6548af50f4e1a0377c76372e83321c3ed8c5aec
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-08-21 16:59:43 +0530

    Update app.js

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const blogsRouter = require('./controllers/blogs.js')
+const mongoose = require('mongoose')
+const config = require('./utils/config.js')
 
 const app = express()
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 module.exports = app
