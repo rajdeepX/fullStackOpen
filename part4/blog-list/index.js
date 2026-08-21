255e38e7e8b0f851c987cc7ab119127c6442ccd7
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-08-21 16:59:35 +0530

    Update index.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,6 +1,7 @@
 const app = require('./app.js')
+const config = require('./utils/config.js')
 
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
