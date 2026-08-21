79f1f31fa4e02d8ec4524e9743de84155205ed79
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-08-21 16:59:40 +0530

    Update blog.js

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -7,11 +7,6 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 })
 
-const password = process.argv[2]
-
-const mongoUrl = `mongodb://fullstackopen:${password}@ac-z8l1ddy-shard-00-00.hxndo4v.mongodb.net:27017,ac-z8l1ddy-shard-00-01.hxndo4v.mongodb.net:27017,ac-z8l1ddy-shard-00-02.hxndo4v.mongodb.net:27017/blogListApp?ssl=true&replicaSet=atlas-qqtb2i-shard-0&authSource=admin&appName=Cluster0`
-mongoose.connect(mongoUrl, { family: 4 })
-
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
